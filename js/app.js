9d5017fe881d04575a1107199b2bd74b66a5384b
rimonspi12 <rimonmpi@gmail.com>
2021-09-02 14:11:53 +0600

    text find

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 const searchBook = () => {
-    const serachInput = document.getElementById('input-field');
-    const searchText = serachInput.value;
-    serachInput.value = '';
-    // div for error message show browser;
-    const errorDiv = document.getElementById('error-container');
-    const div = document.createElement('div');
-    div.classList.add('my-style');
+    const searchInput = document.getElementById('input-field');
+    const searchText = searchInput.value;
+    // console.log(searchText);
+    // fetch('')
+    // console.log('user data function is called');
+    userData(searchText);
+    searchInput.value = '';
+
+}
+
+const userData = data => {
+    // console.log(data);
+    const url = `https://openlibrary.org/search.json?q=${data}`;
+    // console.log(url);
+    const addDiv = document.getElementById('add-detail');
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.docs[0].title_suggest, data.docs[0].author_name, data.docs[2].publish_place, data.docs[0].first_publish_year, data.docs[0].author_name, data.docs[3].cover_i));
 }
